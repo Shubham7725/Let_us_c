5d96c6cd46e069bb24be089282bcfd5805529d60
Shubham7725 <sharma93187@gmail.com>
2023-12-06 12:35:07 +0530

    commit

--- Unit_3/exercise-B/i.c
+++ Unit_3/exercise-B/i.c
@@ -2,6 +2,28 @@
 // is the difference between the smallest and biggest number in
 // the list.
 #include <stdio.h>
+#include <limits.h>
 int main()
 {
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+    int totalNumbers;
+    printf("How many numbers you wants to give:");
+    scanf("%d", &totalNumbers);
+    for (int i = 1; i <= totalNumbers; i++)
+    {
+        int number;
+        printf("enter the number:");
+        scanf("%d", &number);
+        if (number < mini)
+        {
+            mini = number;
+        }
+        else if (number > maxi)
+        {
+            maxi = number;
+        }
+    }
+    int range = maxi - mini;
+    printf("range is %d", range);
 }
