c97a1b6a841de07dcea1500ad502740189b428cf
Shubham <sharma93187@gmail.com>
2023-12-06 09:55:40 +0530

    Delete Unit_3 directory

--- Unit_3/d.c
+++
@@ -1,9 +0,0 @@
-// Write a program to print all the ASCII values and their
-// equivalent characters using a while loop. The ASCII values
-// vary from 0 to 255.
-#include <stdio.h>
-int main()
-{
-    for (int i = 0; i < 256; i++)
-        printf("The ascii value  for %d is  %c\n", i, i);
-}
