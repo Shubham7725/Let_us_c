5add5bd7c48c3e9d6d243bcbe87af728c42e7b1b
Shubham7725 <sharma93187@gmail.com>
2023-12-06 11:57:45 +0530

    commit

--- Unit_3/exercise-B/h.c
+++ Unit_3/exercise-B/h.c
@@ -1,6 +1,18 @@
 // Write a program to find the octal equivalent of the entered
 // number.
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+    int number, octal = 0, i = 1;
+    printf("enter the decimal number:");
+    scanf("%d", &number);
+    while (number != 0)
+    {
+        int remainder = number % 8;
+        octal = octal + remainder * i;
+        number = number / 8;
+        i = i * 10;
+    }
+    printf("octal is %d", octal);
 }
